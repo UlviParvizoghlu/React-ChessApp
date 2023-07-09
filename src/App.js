4854b28ad88bf79ccb0c2d77c6955b0ed879afe2
Ulvi Spaethe <ulvispaethe@gmail.com>
2023-07-09 22:27:06 +0100

    Finish

--- src/App.js
+++ src/App.js
@@ -7,10 +7,19 @@ import { Helmet } from 'react-helmet';
 
 function App() {
   const [board, setBoard] = useState([]);
+  const [isGameOver, setIsGameOver] = useState();
+  const [result, setResult] = useState();
+
 
   useEffect(() => {
     initGame()
-    const subscribe = subjectGame.subscribe((sub) => setBoard(sub.chess));
+    const subscribe = subjectGame.subscribe((sub) =>{
+
+      setBoard(sub.chess)
+      setIsGameOver(sub.isGameOver)
+      setResult(sub.result)
+
+    }  );
 
     return () => {
       subscribe.unsubscribe();
@@ -22,7 +31,13 @@ function App() {
       <Helmet>
         <title>Chess App</title>
       </Helmet>
-      <div className='bg-black h-screen flex justify-center items-center'>
+      <div className='bg-black h-screen flex justify-center items-center relative'>
+        {
+          isGameOver && <div className='absolute bg-white bg-opacity-80 rounded-lg p-3 flex flex-col justify-center items-center '>
+            <h1 className='font-extrabold'>Game Over !</h1>
+            {result && <div className='font-bold'>{result}</div>}
+          </div>
+        }
         <Board board={board} />
       </div>
     </DndProvider>
--- src/Game.js
+++ src/Game.js
@@ -19,5 +19,27 @@ export const move = (from, to) => {
 };
 
 const updateGame = () => {
-  subjectGame.next({ chess: chess.board() });
+  const isGameOver = chess.isGameOver()
+  subjectGame.next({ chess: chess.board(), isGameOver, result: isGameOver ? getGameResult() : null});
 };
+
+const getGameResult = () =>{
+    if(chess.isCheckmate()){
+        const winner = chess.turn() === 'w' ? 'Black' : 'White';
+        return `ŞAH MAT - Winner : ${winner}`
+    }else if(chess.isDraw()){
+        let reason = "50 həmlə qaydası";
+        if(chess.isStalemate()){
+            reason = 'Çıxmaz döngü'
+        }else if(chess.isThreefoldRepetition()){
+            reason = '3-dən çox təkrarlama'
+        }else if(chess.isInsufficientMaterial()){
+            reason = "Yetərsiz hərəkət"
+        }
+        return reason;
+
+    }else{
+        return 'Bilinməyən vəziyyət'
+    }
+
+}
